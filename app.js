2eacb48ec0e31acc07e9e5ae5131d22af9c8a71b
mei3434 <meiko3434@gmail.com>
2026-08-06 07:54:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("🌳 よめるん！スタート！");
+document.getElementById("startBtn").onclick=function(){
+
+    alert("🌳 よめるん！へようこそ！\n\n次回から漢字クイズが始まります😊");
+
+}
+
+document.getElementById("reviewBtn").onclick=function(){
+
+    alert("この機能は次回追加します⭐");
+
+}
+
+document.getElementById("scoreBtn").onclick=function(){
+
+    alert("せいせき画面は次回追加します📊");
+
+}
